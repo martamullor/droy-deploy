9e4e3246103255671c583620e60bf382d813451f
Marc Serra <mserrahidalgo@gmail.com>
2020-05-25 20:19:19 +0200

    Delete editing component properties

--- src/components/droy/ImageEditable.js
+++ src/components/droy/ImageEditable.js
@@ -2,21 +2,15 @@ import React, { Component } from 'react'
 
 export default class ImageEditable extends Component {
 
-  uploadHandler = () => {
-    this.refs.imageUploader.click();
-  }
-
   handleError = (e) => {
     e.target.src = '/img/notFound.jpg'
   }
 
   render () {
-    const { src, changeImage, name, style } = this.props
-    const dataId = this.props['data-id']
+    const { src, style } = this.props
     return (
       <div style={style}>
-        <input data-id={dataId} name={name} id={name} style={{ display: 'none' }} onChange={changeImage} ref="imageUploader" type="file"/>
-        <img onError={this.handleError} onDoubleClick={this.uploadHandler} src={src} alt="" style={{ width: '100%', height: '100%' }}/>
+        <img onError={this.handleError} src={src} alt="" style={{ width: '100%', height: '100%' }}/>
       </div>
     )
   }
--- src/components/droy/LinkEditable.js
+++ src/components/droy/LinkEditable.js
@@ -3,16 +3,15 @@ import React, { Component } from 'react'
 export default class LinkEditable extends Component {
 
   goToLink = () => {
-    const { info, mode } = this.props
-    if(mode !== "view") return
+    const { info } = this.props
     if(info.toNewPage) window.open(info.href)
     else window.location = info.href
   }
 
   render () {
-    let { info, style, onDoubleClick, contentAttrStyle } = this.props
+    let { info, style, contentAttrStyle } = this.props
     const dataId = this.props['data-id']
     style = Object.assign({}, style, contentAttrStyle)
-    return <button type="button" onClick={this.goToLink} data-id={dataId} onDoubleClick={onDoubleClick} style={style}>{info.text}</button>
+    return <button type="button" onClick={this.goToLink} data-id={dataId} style={style}>{info.text}</button>
   }
 }
